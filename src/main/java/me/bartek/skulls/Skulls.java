218080b43681c92c6dea2abf557c3937d842f525
xketrab <66796393+xketrab@users.noreply.github.com>
2021-03-06 16:44:20 +0100

    Initial commit

---
+++ src/main/java/me/bartek/skulls/Skulls.java
@@ -0,0 +1,17 @@
+package me.bartek.skulls;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skulls extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
