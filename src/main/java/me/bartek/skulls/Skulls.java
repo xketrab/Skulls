3e6b93f8dfec111d7a6801da3927069e2f1535bb
kacper-laptop <bartek.turniak@interia.pl>
2021-03-11 14:28:07 +0100

    test

--- src/main/java/me/bartek/skulls/Skulls.java
+++ src/main/java/me/bartek/skulls/Skulls.java
@@ -23,6 +23,8 @@ public class Skulls extends JavaPlugin implements CommandExecutor {
         this.getServer().getPluginManager().registerEvents(new DeathListener(this), this);
         this.saveDefaultConfig();
 
+        // testing if this works.
+
         this.getCommand("skull").setExecutor(new SkullCommand(this));
         this.getCommand("skull").setTabCompleter(new SkullCommandTabCompleter(this));
 
